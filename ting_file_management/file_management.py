da05b54b734260bf651ada141c146c43af4642db
Paulo Victor <paulo.v.prado@hotmail.com>
2023-06-19 19:40:23 -0300

    feat: adiciona a função txt_importer que importar notícias a partir de um arquivo TXT

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,16 @@
+import sys
+
 def txt_importer(path_file):
-    """Aqui irá sua implemenetação"""
+    if not path_file.endswith(".txt"):
+        sys.stderr.write("Formato inválido\n")
+        return []
+
+    try: 
+        file = open(path_file, "r")
+        reading = file.read()
+        lines = reading.split("\n")
+        file.close()
+        return lines
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+        return []
